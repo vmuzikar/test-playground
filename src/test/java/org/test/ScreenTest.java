4d426028a5c6a622fef2f35fcfe091d51bc62f38
Vaclav Muzikar <vmuzikar@redhat.com>
2017-07-24 17:05:33 +0200

    Init commit

---
+++ src/test/java/org/test/ScreenTest.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Vaclav Muzikar <vmuzikar@redhat.com>
+ */
+@RunWith(Arquillian.class)
+public class ScreenTest {
+    @Drone
+    private WebDriver driver;
+
+    @Test
+    public void screenshot() throws Exception {
+        driver.manage().window().maximize();
+        driver.navigate().to("http://www.google.com");
+        Thread.sleep(5000);
+    }
+}
