ef157ceb6c246d03f667dad6d841ca6e9c20ef41
vmuzikar <vmuzikar@redhat.com>
2018-04-24 19:20:06 +0200

    DockerTest

---
+++ src/test/java/org/test/DockerTest.java
@@ -0,0 +1,27 @@
+package org.test;
+
+import org.junit.Test;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Vaclav Muzikar <vmuzikar@redhat.com>
+ */
+public class DockerTest {
+    @Test
+    public void cmdOutputTest() throws Exception {
+        final String testStr = "Hello World!";
+
+        GenericContainer container = new GenericContainer("centos:latest")
+                .withCommand("sleep infinity");
+        container.start();
+
+        Container.ExecResult result = container.execInContainer("echo", testStr);
+
+        System.out.printf("STDOUT: %s\n---\nSTDERR: %s", result.getStdout(), result.getStderr());
+
+        assertEquals(testStr + "\n", result.getStdout());
+    }
+}
