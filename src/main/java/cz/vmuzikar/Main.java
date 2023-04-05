c9d45d1ad482a4483c9945b8612c0d7bf45eda69
Václav Muzikář <vmuzikar@redhat.com>
2023-04-05 16:34:23 +0200

    init

--- src/main/java/cz/vmuzikar/Main.java
+++ src/main/java/cz/vmuzikar/Main.java
@@ -1,6 +1,9 @@
 package cz.vmuzikar;
 
-import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import jakarta.inject.Inject;
 
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -9,21 +12,33 @@ import java.util.stream.Collectors;
  * @author Vaclav Muzikar <vmuzikar@redhat.com>
  */
 public class Main {
-    public static void main(String[] args) throws Exception {
-        final String namespace = "keycloak-test-db2cd0cb-c666-47ef-afd8-a1db3cf7c16e";
+    public static void main(String[] args) {
+        Quarkus.run(MyApp.class, args);
+    }
 
-        var client = new KubernetesClientBuilder().build();
-        var pods = client.pods().inNamespace(namespace).list().getItems().stream()
-                        .map(p -> p.getMetadata().getName())
-                        .collect(Collectors.toList());
+    public static class MyApp implements QuarkusApplication {
+        @Inject
+        KubernetesClient client;
 
-        System.out.println(pods);
+        @Override
+        public int run(String... args) throws Exception {
+            final String namespace = "keycloak-test-2b4c6f1a-ceb1-426e-b3c7-293a769d84fb";
 
-        var pf = client.pods().inNamespace(namespace).withName(pods.get(0)).portForward(8443, 8443);
+//            var client = new KubernetesClientBuilder().build();
+            var pods = client.pods().inNamespace(namespace).list().getItems().stream()
+                    .map(p -> p.getMetadata().getName())
+                    .collect(Collectors.toList());
 
-        while (!new Scanner(System.in).hasNextLine()) {}
+            System.out.println(pods);
 
-        pf.close();
-        client.close();
+            var pf = client.pods().inNamespace(namespace).withName(pods.get(0)).portForward(8443, 8443);
+
+            while (!new Scanner(System.in).hasNextLine()) {}
+
+            pf.close();
+            client.close();
+            Quarkus.blockingExit();
+            return 0;
+        }
     }
 }
